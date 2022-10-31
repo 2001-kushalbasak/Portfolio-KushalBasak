5b15f73010786a7b209ae28d06dcec788fcf4c08
Kushal <kushal.basak.uemk.cs.2023@gmail.com>
2022-10-31 20:25:35 +0530

    1st commit

---
+++ myblog/Datastruc/Dsa.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="Dsa.css">
+  <title>My Website</title>
+</head>  
+    
+    <h1>Welcome To MyBlog.com// DSA In Java</h1>
+<body>
+    
+    
+    <div class="blog-entry">
+      <img class="blog-img" src="https://upload.wikimedia.org/wikipedia/commons/4/42/Otvorena_knjiga.JPG" alt="">
+      <div class="content-date"><div class="date">2022-08-02</div></div>
+      <div class="blog-content">
+        <div class="content-title"><h1>Array All Code</h1></div>
+        <div class="content-tag">CS</div>
+        <span class="content-text">Lorem ipsum dolor
+          sit amet, consectetur adipiscing elit. Cra
+          s lacinia consectetur arcu, in semper magn
+          a posuere non. Fusce sagitti</span>
+          <div class="content-button">
+              <a href="array.html">Read more</a>
+              </div>
+      </div>
+    </div>
+    
+    
+    <div class="blog-entry">
+      <img class="blog-img" src="https://upload.wikimedia.org/wikipedia/commons/4/42/Otvorena_knjiga.JPG" alt="">
+      <div class="content-date"><div class="date">2022-08-02</div></div>
+      <div class="blog-content">
+        <div class="content-title"><h1>Linkedlist All Code</h1></div>
+        <div class="content-tag">CS</div>
+        <span class="content-text">Lorem ipsum dolor
+          sit amet, consectetur adipiscing elit. Cra
+          s lacinia consectetur arcu, in semper magn
+          a posuere non. Fusce sagitti</span>
+          <div class="content-button">
+              <a href="#"></a>
+              Read more</div>
+      </div>
+    </div>
+    
+    
+    <div class="blog-entry">
+      <img class="blog-img" src="https://upload.wikimedia.org/wikipedia/commons/4/42/Otvorena_knjiga.JPG" alt="">
+      <div class="content-date"><div class="date">2022-08-02</div></div>
+      <div class="blog-content">
+        <div class="content-title"><h1>Stack All Code</h1></div>
+        <div class="content-tag">CS</div>
+        <span class="content-text">Lorem ipsum dolor
+          sit amet, consectetur adipiscing elit. Cra
+          s lacinia consectetur arcu, in semper magn
+          a posuere non. Fusce sagitti</span>
+          <div class="content-button">
+              <a href="#"></a>
+              Read more</div>
+      </div>
+    </div>
+    
+    
+    <div class="blog-entry">
+      <img class="blog-img" src="https://upload.wikimedia.org/wikipedia/commons/4/42/Otvorena_knjiga.JPG" alt="">
+      <div class="content-date"><div class="date">2022-08-02</div></div>
+      <div class="blog-content">
+        <div class="content-title"><h1>Queue All Code</h1></div>
+        <div class="content-tag">CS</div>
+        <span class="content-text">Lorem ipsum dolor
+          sit amet, consectetur adipiscing elit. Cra
+          s lacinia consectetur arcu, in semper magn
+          a posuere non. Fusce sagitti</span>
+          <div class="content-button">
+              <a href="#"></a>
+              Read more</div>
+      </div>
+    </div>
+   
+  </body>
